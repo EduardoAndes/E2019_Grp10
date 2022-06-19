bf57b617e633544ac281d771eb3e78b7152f03eb
Eduardo Andes III <94593548+EduardoAndes@users.noreply.github.com>
2022-06-19 20:20:26 +0800

    updated

---
+++ WaterCooperative/DAL/UsersDAL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Dapper;
+
+namespace DAL
+{
+    public class UsersDAL
+    {
+        public static Users CheckLogin(Users obj)
+        {
+            string sqlQuery = "SELECT Password, Username FROM Users WHERE(Password = N'admin') AND (Username = N'admin')";
+            using (var con = ConnectionUtil.GetConnection())
+            {
+                return con.Query<Users>(sqlQuery, obj).FirstOrDefault();
+            }
+        }
+    }
+}
---
+++ WaterCooperative/WaterCooperative/Login.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Domain;
+using DAL;
+
+namespace WaterCooperative
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(txtUsername.Text.Trim()=="" || txtPassword.Text.Trim() == "")
+            {
+                this.lblError.Text = "Please Enter Username and Password!";
+            }
+            else
+            {
+                this.lblError.Text = "";
+
+                var username = txtUsername.Text.Trim();
+                var password = txtPassword.Text.Trim();
+
+                var obj = new Users
+                {
+                   Username = username,
+                   Password = password
+                };
+                var user = UsersDAL.CheckLogin(obj);
+
+                if (user != null)
+                {
+                    Response.Redirect("Home.aspx");
+                }
+                else
+                {
+                    lblError.Text = "Invalid Username or Password!";
+                }
+            }
+        }
+    }
+}
